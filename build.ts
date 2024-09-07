36b0ba52f3592bce4125fc9e09a530c78e9ae3d4
Benjamin Wilkins <62174924+benjamin-wilkins@users.noreply.github.com>
2024-09-07 21:32:10 +0100

    Edit publish pipeline and publish 0.0.2

--- build.ts
+++ build.ts
@@ -1,12 +1,22 @@
 import { build } from "npm:esbuild"
 import { denoPlugins } from "jsr:@luca/esbuild-deno-loader"
 
-await build({
-  plugins: [...denoPlugins({
-    importMapURL: import.meta.resolve("./deno.json")
-  })],
-  entryPoints: ["src/mod.ts"],
-  outdir: "dist",
-  bundle: true,
-  format: "esm"
-})
+await Promise.all([
+  build({
+    plugins: [...denoPlugins({
+      importMapURL: import.meta.resolve("./deno.json")
+    })],
+    entryPoints: ["src/mod.ts"],
+    outdir: "dist",
+    bundle: true,
+    format: "esm"
+  }),
+  Deno.copyFile(
+    "./README.md",
+    "./dist/README.md"
+  ),
+  Deno.copyFile(
+    "./LICENSE",
+    "./dist/LICENSE"
+  )
+])
